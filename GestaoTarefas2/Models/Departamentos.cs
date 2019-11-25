3264b47d143067ad79610963d0b29bb34493943a
dylan pinto <dylan-pinto@hotmail.com>
2019-11-25 12:51:32 +0000

    departamentos

---
+++ GestaoTarefas2/Models/Departamentos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefas2.Models
+{
+    public class Departamentos
+    {
+        public int DepartamentoId { get; set; }
+        [Required]
+        public string Nome { get; set; }
+
+    }
+  
+}
